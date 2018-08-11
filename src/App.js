e95c4ecc7d1ae17a48ba3a9e5efea60b819310b7
Riza Fahmi <rizafahmi@gmail.com>
2018-08-11 18:28:08 +0700

    Usign static state for data

--- src/App.js
+++ src/App.js
@@ -2,6 +2,32 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      podcasts: [
+        {
+          id: 1,
+          thumbnail:
+            'https://s3-us-west-2.amazonaws.com/anchor-generated-image-bank/production/podcast_uploaded_nologo400/405469/405469-1522117397083-c0592d270d967.jpg',
+          title: 'Ceritanya Developer Podcast',
+        },
+        {
+          id: 2,
+          thumbnail:
+            'https://s3-us-west-2.amazonaws.com/anchor-generated-image-bank/production/podcast_uploaded400/89375/89375-1526983608105-f58a2aadf7bc1.jpg',
+          title: 'Developer Muslim',
+        },
+        {
+          id: 3,
+          thumbnail:
+            'https://s3-us-west-2.amazonaws.com/anchor-generated-image-bank/production/podcast_uploaded_nologo400/814282/814282-1532652306173-4bb60a18c1dfb.jpg',
+          title: 'Data Pods',
+        },
+      ],
+    };
+  }
+
   render() {
     return (
       <div className="wrapper">
@@ -17,18 +43,17 @@ Search
           </form>
         </div>
         <div className="podcasts">
-          <div className="podcast">
-            <img
-              alt="wicara"
-              src="https://s3-us-west-2.amazonaws.com/anchor-generated-image-bank/production/podcast_uploaded_episode400/779648/779648-1532835833639-5d40d846bf169.jpg"
-            />
-            <h2>
-Ep 02. Introduction to Programming - Riza Fahmi
-            </h2>
-            <a className="button">
+          {this.state.podcasts.map(podcast => (
+            <div key={podcast.id} className="podcast">
+              <img alt={podcast.title} src={podcast.thumbnail} />
+              <h2>
+                {podcast.title}
+              </h2>
+              <a className="button">
 Listen
-            </a>
-          </div>
+              </a>
+            </div>
+          ))}
         </div>
       </div>
     );
