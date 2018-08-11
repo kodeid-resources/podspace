ace4a4b478637debaafd1261221480cd59c918fb
Riza Fahmi <rizafahmi@gmail.com>
2018-08-11 18:11:20 +0700

    Add theme

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="wrapper">
+        <h1>
+PodSpace
+        </h1>
+        <div className="search-bar">
+          <form>
+            <input type="text" placeholder="Search for podcast" />
+            <button>
+Search
+            </button>
+          </form>
+        </div>
+        <div className="podcasts">
+          <div className="podcast">
+            <img
+              alt="wicara"
+              src="https://s3-us-west-2.amazonaws.com/anchor-generated-image-bank/production/podcast_uploaded_episode400/779648/779648-1532835833639-5d40d846bf169.jpg"
+            />
+            <h2>
+Ep 02. Introduction to Programming - Riza Fahmi
+            </h2>
+            <a className="button">
+Listen
+            </a>
+          </div>
+        </div>
       </div>
     );
   }
