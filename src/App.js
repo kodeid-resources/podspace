36e5594baffd96bba9f89e45923d6261ad792d67
Riza Fahmi <rizafahmi@gmail.com>
2018-08-19 18:13:04 +0700

    Event onChange

--- src/App.js
+++ src/App.js
@@ -1,48 +1,57 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Podcast from './components/Podcast.js';
-import './App.css';
+import Podcast from "./components/Podcast.js";
+import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
+      searchTerm: "",
       podcasts: [
         {
           id: 1,
           thumbnail:
-            'https://s3-us-west-2.amazonaws.com/anchor-generated-image-bank/production/podcast_uploaded_nologo400/405469/405469-1522117397083-c0592d270d967.jpg',
-          title: 'Ceritanya Developer Podcast',
+            "https://s3-us-west-2.amazonaws.com/anchor-generated-image-bank/production/podcast_uploaded_nologo400/405469/405469-1522117397083-c0592d270d967.jpg",
+          title: "Ceritanya Developer Podcast"
         },
         {
           id: 2,
           thumbnail:
-            'https://s3-us-west-2.amazonaws.com/anchor-generated-image-bank/production/podcast_uploaded400/89375/89375-1526983608105-f58a2aadf7bc1.jpg',
-          title: 'Developer Muslim',
+            "https://s3-us-west-2.amazonaws.com/anchor-generated-image-bank/production/podcast_uploaded400/89375/89375-1526983608105-f58a2aadf7bc1.jpg",
+          title: "Developer Muslim"
         },
         {
           id: 3,
           thumbnail:
-            'https://s3-us-west-2.amazonaws.com/anchor-generated-image-bank/production/podcast_uploaded_nologo400/814282/814282-1532652306173-4bb60a18c1dfb.jpg',
-          title: 'Data Pods',
-        },
-      ],
+            "https://s3-us-west-2.amazonaws.com/anchor-generated-image-bank/production/podcast_uploaded_nologo400/814282/814282-1532652306173-4bb60a18c1dfb.jpg",
+          title: "Data Pods"
+        }
+      ]
     };
   }
 
   handleSearchButton() {
-    console.log('Searching...');
+    console.log("Searching...");
   }
+
+  handleSearchInput = event => {
+    this.setState({
+      searchTerm: event.target.value
+    });
+  };
 
   render() {
     return (
       <div className="wrapper">
-        <h1>
-PodSpace
-        </h1>
+        <h1>PodSpace</h1>
         <div className="search-bar">
           <form>
-            <input type="text" placeholder="Search for podcast" />
+            <input
+              type="text"
+              placeholder="Search for podcast"
+              onChange={this.handleSearchInput}
+            />
             <button onClick={this.handleSearchButton} type="button">
               Search
             </button>
