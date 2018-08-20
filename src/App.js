88bcdfbb814847bf36eeb07290ab44c14818b993
Riza Fahmi <rizafahmi@gmail.com>
2018-08-20 14:48:52 +0700

    Fetch data using axios

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 import { css } from "emotion";
 
 import Podcast from "./components/Podcast.js";
@@ -18,27 +19,16 @@ class App extends Component {
       ui: {
         formVisibility: false
       },
-      podcasts: [
-        {
-          id: 1,
-          thumbnail:
-            "https://s3-us-west-2.amazonaws.com/anchor-generated-image-bank/production/podcast_uploaded_nologo400/405469/405469-1522117397083-c0592d270d967.jpg",
-          title: "Ceritanya Developer Podcast"
-        },
-        {
-          id: 2,
-          thumbnail:
-            "https://s3-us-west-2.amazonaws.com/anchor-generated-image-bank/production/podcast_uploaded400/89375/89375-1526983608105-f58a2aadf7bc1.jpg",
-          title: "Developer Muslim"
-        },
-        {
-          id: 3,
-          thumbnail:
-            "https://s3-us-west-2.amazonaws.com/anchor-generated-image-bank/production/podcast_uploaded_nologo400/814282/814282-1532652306173-4bb60a18c1dfb.jpg",
-          title: "Data Pods"
-        }
-      ]
+      podcasts: []
     };
+  }
+  async componentDidMount() {
+    const res = await axios.get(
+      "https://podspace-server-aeffazgssu.now.sh/podcasts"
+    );
+    this.setState({
+      podcasts: res.data
+    });
   }
 
   handleSearchButton() {
